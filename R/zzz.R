e448ee4d9b16dfa17099e8884f4646378b309696
kdoub5ha <kdoub5ha@gmail.com>
2019-05-02 11:31:46 -0700

    Create zzz.R

---
+++ R/zzz.R
@@ -0,0 +1,12 @@
+.onAttach <- function(libname, pkgname){
+  options(stringsAsFactors = F)   
+  
+  library(glmnet)
+  library(plyr)
+  library(dplyr)
+  library(RColorBrewer)
+  library(e1071)
+  library(ggplot2)
+  library(Hmisc)
+  library(randomForest)
+  }
