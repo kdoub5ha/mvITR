b4f9855d7f501332b06bf41f498638d2aeaf1b5f
kdoub5ha <kdoub5ha@gmail.com>
2019-05-02 11:35:26 -0700

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,6 @@
 .onAttach <- function(libname, pkgname){
   options(stringsAsFactors = F)   
-  
   library(glmnet)
-  library(plyr)
-  library(dplyr)
-  library(RColorBrewer)
-  library(e1071)
-  library(ggplot2)
   library(Hmisc)
   library(randomForest)
   }
